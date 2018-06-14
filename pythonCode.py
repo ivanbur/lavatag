f5f57dfdbb3d5c54728068287cc954bded28337a
Ivan Burgert <ivanburgert@Ivans-MacBook-Pro.local>
2018-06-14 08:42:48 -0700

    removed some images and commented out something else

--- pythonCode.py
+++ pythonCode.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-@app.route("/favicon.ico")
-def favicon():
-	return send_from_directory(os.path.join(app.root_path, "static", "images"), "favicon.ico", mimetype="image/png")
+# @app.route("/favicon.ico")
+# def favicon():
+# 	return send_from_directory(os.path.join(app.root_path, "static", "images"), "favicon.ico", mimetype="image/png")
   
   
   
